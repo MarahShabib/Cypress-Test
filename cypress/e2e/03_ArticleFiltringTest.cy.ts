385ab7206abc559b0f14560b152b118b1a1ed30e
MarahShabib <47115227+MarahShabib@users.noreply.github.com>
2025-08-19 22:41:49 +0300

    Clean Code

--- cypress/e2e/03_ArticleFiltringTest.cy.ts
+++ cypress/e2e/03_ArticleFiltringTest.cy.ts
@@ -10,16 +10,13 @@ describe('Article Category Filter Tests', () => {
   it('should filter articles by Animals category', () => {
    
     cy.contains('Animals').click();
-    cy.get('article').each(($el) => {
-      cy.wrap($el).should('exist'); // all articles should be visible
-    });
+    cy.get('article').should('have.length.greaterThan', 0);
     cy.contains('Pets').click();
     cy.url().should('include', '/animals/topic/pets');
   });
 
   it('should filter articles by Science category', () => {
 
-   
     cy.contains('Science & Nature').click();
     cy.get('article').should('have.length.greaterThan', 0);
     cy.contains('Oceans').click();
